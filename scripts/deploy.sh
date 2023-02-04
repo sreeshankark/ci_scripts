91d831908c8a050aa04f0a9c4ed7117942ab33ed
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-05 00:31:43 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,15 +13,17 @@ cp recovery.img "upload/$CR_NAME";
 cd "upload/$CR_NAME/$DATE";
 cd upload;
 cd $CR_NAME;
-git init;
-git remote add origin https://github.com/Sanju0910/releases.git;
-git branch -M $CR_NAME;
-git fetch;
-git pull origin $CR_NAME;
-git add .
-git commit -m "$CR_NAME : $DATE";
-git push origin $CR_NAME;
-echo $USERNAME
-sleep 6;
-echo $GH_TOKEN
+# Final Release
+	ghr -t ${GH_BOT_TOKEN} -u ${GH_USER} -r ${GH_REPO} -n "$(echo $CR) Release for $(echo $CODENAME)" -b "PBRP $(echo $RELEASE_TAG)"
+#git init;
+#git remote add origin https://github.com/Sanju0910/releases.git;
+#git branch -M $CR_NAME;
+#git fetch;
+#git pull origin $CR_NAME;
+#git add .
+#git commit -m "$CR_NAME : $DATE";
+#git push origin $CR_NAME;
+#echo $USERNAME
+#sleep 6;
+#echo $GH_TOKEN
 exit 0;
