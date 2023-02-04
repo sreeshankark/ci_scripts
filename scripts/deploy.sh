c766d22893c8801cfd1d7439be00b679ab3ef405
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-04 22:48:09 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-cd "~/$CR_NAME/out/target/product/$CODENAME";
-date
-echo spawn git.sh > git.sh;
-echo expect "Username for 'https://github.com':" >> git.sh;
-echo send $USERNAME >> git.sh;
-echo sleep 3 >> git.sh;
-echo spawn git.sh >> git.sh;
-echo expect "Password for 'https://$USERNAME@github.com':" >> git.sh;
-echo send $GH_TOKEN >> git.sh;
-
-mkdir "upload/$CR_NAME/date +”%d-%b-%Y";
-cp *.zip "upload/$CR_NAME/date +”%d-%b-%Y";
-cp recovery.img "upload/$CR_NAME/date +”%d-%b-%Y";
-cd "upload/$CR_NAME/date +”%d-%b-%Y";
+cd ~
+cd $CR_NAME
+cd out/target/product/$CODENAME;
+DATE=date
+mkdir "upload/$CR_NAME/$DATE";
+cp *.zip "upload/$CR_NAME/$DATE";
+cp recovery.img "upload/$CR_NAME/$DATE";
+cd "upload/$CR_NAME/$DATE";
 git init;
 git remote add origin https://github.com/Sanju0910/releases.git;
 git branch -M $CR_NAME;
 git fetch;
 git pull origin $CR_NAME;
 git add .
-git commit -m "$CR_NAME : date +”%d-%b-%Y”";
+git commit -m "$CR_NAME : $DATE";
 git push origin $CR_NAME;
-sleep 3;
-bash out/target/product/$CODENAME/git.sh;
+sleep 6;
+echo $USERNAME
+sleep 6;
+echo $GH_TOKEN
 exit 0;
