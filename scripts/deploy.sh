b87bb1ae9d52de57106835d8c9781e932a44a843
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-04 23:40:20 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,11 +3,16 @@
 cd ~
 cd $CR_NAME
 cd out/target/product/$CODENAME;
-DATE=date
-mkdir "upload/$CR_NAME/$DATE";
-cp *.zip "upload/$CR_NAME/$DATE";
-cp recovery.img "upload/$CR_NAME/$DATE";
+mkdir upload;
+cd upload;
+mkdir $CR_NAME;
+cd $CR_NAME;
+cd ../..
+cp *.zip "upload/$CR_NAME";
+cp recovery.img "upload/$CR_NAME";
 cd "upload/$CR_NAME/$DATE";
+cd upload;
+cd $CR_NAME;
 git init;
 git remote add origin https://github.com/Sanju0910/releases.git;
 git branch -M $CR_NAME;
@@ -16,7 +21,6 @@ git pull origin $CR_NAME;
 git add .
 git commit -m "$CR_NAME : $DATE";
 git push origin $CR_NAME;
-sleep 6;
 echo $USERNAME
 sleep 6;
 echo $GH_TOKEN
