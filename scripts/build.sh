cf71e2ed37d483d364641e7963508ce8d7efab55
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-05 12:02:46 +0530

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 cd ~/${CR_NAME};
 source build/envsetup.sh;
 
-lunch twrp_$CODENAME-eng || lunch omni_${CODENAME}-eng || { printf "Compilation failed.\n"; exit 1; }
+lunch twrp_$CODENAME-eng || lunch omni_$CODENAME-eng || { printf "Compilation failed.\n"; exit 1; }
 
 mka $TARGET -j$(nproc --all);
 exit 0;
