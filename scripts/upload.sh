c5cf5ef5a34fa5551d2a9843f5a1964b1a3b85eb
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-05 12:34:36 +0530

    Update upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd ~
-cd $CR_NAME/out/target/product/$CODENAME;
+cd $CR_NAME
+cd out/target/product/$CODENAME;
 curl -sL https://git.io/file-transfer | sh;
 ./transfer wet recovery.img;
 ./transfer wet *.zip;
