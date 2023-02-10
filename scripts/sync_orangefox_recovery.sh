d30cb9e88d4f271394568402ef0f6f9ae2f6fda3
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-10 21:50:09 +0530

    Update sync_orangefox_recovery.sh

--- scripts/sync_orangefox_recovery.sh
+++ scripts/sync_orangefox_recovery.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-cd ~
+cd /root
 git config --global user.name "$USERNAME"
 git config --global user.email "$USERMAIL"
 
-mkdir ~/OrangeFox_sync
-cd ~/OrangeFox_sync
+mkdir /root/OrangeFox_sync
+cd /root/OrangeFox_sync
 git clone https://gitlab.com/OrangeFox/sync.git
-cd ~/OrangeFox_sync/sync/
-./orangefox_sync.sh --branch 12.1 --path ~/orangefox
-cd ~/orangefox
+cd /root/OrangeFox_sync/sync/
+./orangefox_sync.sh --branch 12.1 --path /root/orangefox
+cd /root/orangefox
 git clone https://github.com/Sanju0910/orangefox_device_oneplus_avicii.git -b android-12.1 device/oneplus/avicii
 
 exit 0
