9ecfe5fc63bcc42c8fa6850ed406153e71d78b4e
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-10 20:58:13 +0530

    Update build_recovery.sh

--- scripts/build_recovery.sh
+++ scripts/build_recovery.sh
@@ -2,15 +2,15 @@
 
 cd ~
 cd ${CR_NAME}
-source build/envsetup.sh;
+source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_VARIANT=${FOX_VARIANT}
 lunch twrp_$CODENAME-eng || lunch omni_$CODENAME-eng || { printf "Compilation failed.\n"; exit 1; }
 
-if [ $TARGET = "pbrp" ];
+if [ $TARGET = "pbrp" ]
 then
-mka $TARGET -j$(nproc --all);
+mka $TARGET -j$(nproc --all)
 else
-mka adbd $TARGET -j$(nproc --all);
+mka adbd $TARGET -j$(nproc --all)
 fi
-exit 0;
+exit 0
