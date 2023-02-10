69d543f749ef314bf1007466dbb744587a3f131f
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-10 21:52:38 +0530

    Update build_recovery.sh

--- scripts/build_recovery.sh
+++ scripts/build_recovery.sh
@@ -4,7 +4,7 @@ cd /root
 cd $CR_NAME
 source build/envsetup.sh
 export ALLOW_MISSING_DEPENDENCIES=true
-export FOX_VARIANT=${FOX_VARIANT}
+export FOX_VARIANT=$FOX_VARIANT
 lunch twrp_$CODENAME-eng || lunch omni_$CODENAME-eng || { printf "Compilation failed.\n"; exit 1; }
 
 if [ $TARGET = "pbrp" ]
