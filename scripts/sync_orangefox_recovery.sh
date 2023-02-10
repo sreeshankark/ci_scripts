fdece23aa33e4012e4297e9104505cf67b829226
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-10 18:44:54 +0530

    Update sync_orangefox_recovery.sh

--- scripts/sync_orangefox_recovery.sh
+++ scripts/sync_orangefox_recovery.sh
@@ -9,6 +9,7 @@ cd ~/OrangeFox_sync
 git clone https://gitlab.com/OrangeFox/sync.git
 cd ~/OrangeFox_sync/sync/
 ./orangefox_sync.sh --branch 12.1 --path ~/${CR_NAME};
+cd ~
 cd $CR_NAME;
 git clone $DT_REPO -b $DT_BRANCH "device/$OEM/$CODENAME";
 
