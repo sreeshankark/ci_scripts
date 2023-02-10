e969af086c4b9375295a8c35493ba7f5231e9ae8
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-10 20:57:25 +0530

    Update sync_orangefox_recovery.sh

--- scripts/sync_orangefox_recovery.sh
+++ scripts/sync_orangefox_recovery.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 cd ~
-git config --global user.name "$USERNAME";
-git config --global user.email "$USERMAIL";
+git config --global user.name "$USERNAME"
+git config --global user.email "$USERMAIL"
 
 mkdir ~/OrangeFox_sync
 cd ~/OrangeFox_sync
 git clone https://gitlab.com/OrangeFox/sync.git
 cd ~/OrangeFox_sync/sync/
-./orangefox_sync.sh --branch 12.1 --path ~/${CR_NAME};
-cd ~/${CR_NAME}
-git clone $DT_REPO -b $DT_BRANCH "device/$OEM/$CODENAME";
+./orangefox_sync.sh --branch 12.1 --path ~/orangefox
+cd ~/orangefox
+git clone $DT_REPO -b $DT_BRANCH "device/$OEM/$CODENAME"
 
 exit 0
