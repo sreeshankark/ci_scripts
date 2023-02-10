b35427b08cc7d70bdad26703dfab8b063974d077
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-02-10 17:35:56 +0530

    Update build_recovery.sh

--- scripts/build_recovery.sh
+++ scripts/build_recovery.sh
@@ -6,5 +6,9 @@ source build/envsetup.sh;
 
 lunch twrp_$CODENAME-eng || lunch omni_$CODENAME-eng || { printf "Compilation failed.\n"; exit 1; }
 
+if [ $TARGET = "pbrp" ];
+then
 mka $TARGET -j$(nproc --all);
+else
+mka adbd $TARGET -j$(nproc --all);
 exit 0;
