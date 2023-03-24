c8fd2f4dff3527e67e4d5bb7840249e77598fc15
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-03-24 05:33:19 +0530

    Update build_rom.sh

--- scripts/build_rom.sh
+++ scripts/build_rom.sh
@@ -8,8 +8,8 @@ source build/envsetup.sh;
 lunch $PRODUCT_NAME-$VARIANT
 
 if [ $ROM_NAME = "evolutionx" ] then
-mka evolution -j$(nproc --all);
+mka evolution -j$(nproc --all)
 else
-mka bacon -j$(nproc --all);
+mka bacon -j$(nproc --all)
 fi
 exit 0;
