fd43479807e5675986efadc8563f2edd581541c4
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-03-28 08:29:56 +0530

    Update sync_rom.sh

--- scripts/sync_rom.sh
+++ scripts/sync_rom.sh
@@ -13,18 +13,18 @@ repo init -u --depth=1 $ROM_REPO -b $ROM_BRANCH;
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
 
 # Clone GREEN source
-git clone $DEVICE_REPO -b $DEVICE_BRANCH "device/$OEM/$CODENAME";
+git clone $DEVICE_REPO --depth=1 -b $DEVICE_BRANCH "device/$OEM/$CODENAME";
 
-git clone $VENDOR_REPO -b $VENDOR_BRANCH "vendor/$OEM/$CODENAME";
+git clone $VENDOR_REPO --depth=1 -b $VENDOR_BRANCH "vendor/$OEM/$CODENAME";
 
-git clone $KERNEL_REPO -b $KERNEL_BRANCH "kernel/$OEM/$CODENAME";
+git clone $KERNEL_REPO --depth=1 -b $KERNEL_BRANCH "kernel/$OEM/$CODENAME";
 
 rm -rf hardware/oplus;
 
-git clone $HARDWARE_REPO -b $HARDWARE_BRANCH "hardware/oplus";
+git clone $HARDWARE_REPO --depth=1 -b $HARDWARE_BRANCH "hardware/oplus";
 
 rm -rf prebuilts/clang/kernel;
 
-git clone https://gitlab.com/Sanju0910/clang-r487747 -b main prebuilts/clang/kernel/linux-x86/clang-r487747;
+git clone https://gitlab.com/Sanju0910/clang-r487747 --depth=1 -b main prebuilts/clang/kernel/linux-x86/clang-r487747;
 
 exit 0;
