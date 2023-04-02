094f0db94e3c46bc146e3b8ab521e76562d4bf74
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-04-02 21:37:17 +0530

    Update build_rom.sh

--- scripts/build_rom.sh
+++ scripts/build_rom.sh
@@ -2,6 +2,17 @@
 
 cd ~
 cd ${ROM_NAME}
+cd ~/roms/${ROM_NAME}
+
+export CCACHE_DIR=~/ccache/${ROM_NAME}/avicii
+
+export CCACHE_EXEC=$(which ccache)
+
+export USE_CCACHE=1
+
+ccache -M 8G
+
+ccache -z
 export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh;
 
