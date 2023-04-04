b5104fc316a40501e92976221298d6724f12ac57
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-04-04 18:15:40 +0530

    Update sync_recovery.sh

--- scripts/sync_recovery.sh
+++ scripts/sync_recovery.sh
@@ -5,10 +5,13 @@ mkdir $CR_NAME;
 cd $CR_NAME;
 git config --global user.name "$USERNAME";
 git config --global user.email "$USERMAIL";
-repo init -u $CR_REPO -b $CR_BRANCH;
+repo init --depth=1 -u $CR_REPO -b $CR_BRANCH;
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags;
-
-git clone $DT_REPO -b $DT_BRANCH "device/$OEM/$CODENAME";
+rm -rf bootable/recovery
+rm -rf system/vold
+git clone https://github.com/PitchBlackRecoveryProject/android_bootable_recovery -b fbe-patches bootable/recovery --depth=1;
+git clone https://github.com/PitchBlackRecoveryProject/android_system_vold -b fbe-patches system/vold --depth=1;
+git clone $DT_REPO -b $DT_BRANCH "device/$OEM/$CODENAME" --depth=1;
 
 exit 0;
