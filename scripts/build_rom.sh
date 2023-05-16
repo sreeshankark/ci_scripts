5e3a70b2db88fa8cc8c3c783534af6906c0a8111
Sreeshankar K <114847933+Sanju0910@users.noreply.github.com>
2023-05-16 13:26:13 +0530

    Update build_rom.sh

--- scripts/build_rom.sh
+++ scripts/build_rom.sh
@@ -2,21 +2,7 @@
 
 cd ~
 cd ${ROM_NAME}
-
-sudo systemctl disable --now systemd-oomd
-export CCACHE_DIR=~/ccache/${ROM_NAME}/avicii
-
-export CCACHE_EXEC=$(which ccache)
-
-export USE_CCACHE=1
-
-ccache -M 10G
-
-ccache -z
-export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh;
-
 lunch $PRODUCT_NAME-$VARIANT
-
-mka evolution -j8
+mka evolution -j$(nproc --all)
 exit 0;
