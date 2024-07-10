7ca66e2e8b4f63cdeab7282e50da05241a6e1ea4
Sreeshankar K <114847933+sreeshankark@users.noreply.github.com>
2024-07-10 21:42:09 +0530

    Update sync_rom.sh

--- scripts/sync_rom.sh
+++ scripts/sync_rom.sh
@@ -6,6 +6,8 @@ cd $ROM_NAME;
 git config --global user.name "$USERNAME";
 git config --global user.email "$USERMAIL";
 
+echo $GH_TOKEN | gh auth login --with-token
+
 # Initialize RED source
 repo init --depth=1 --no-repo-verify -u $ROM_REPO -b $ROM_BRANCH -g default,-mips,-darwin,-notdefault;
 
@@ -15,20 +17,6 @@ repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags --optimized-fetch --pr
 
 # Clone GREEN source
 git clone $DEVICE_REPO --depth=1 -b $DEVICE_BRANCH "device/$OEM/$CODENAME";
-
-git clone $VENDOR_REPO --depth=1 -b $VENDOR_BRANCH "vendor/$OEM/$CODENAME";
-
-git clone $KERNEL_REPO --depth=1 -b $KERNEL_BRANCH "kernel/$OEM/$CODENAME";
-
-rm -rf hardware/oplus;
-
-git clone $HARDWARE_REPO --depth=1 -b $HARDWARE_BRANCH "hardware/oplus";
-
-git clone https://gitlab.com/Sanju0910/vendor_oneplus_apps --depth=1 "vendor/oneplus/apps";
-
-git clone https://github.com/Sanju0910/android_packages_apps_OPlusExtras -b RisingOS "packages/apps/OPlusExtras";
-
-git clone https://github.com/Sanju0910/android_vendor_dolby_atmos "vendor/dolby/atmos";
 
 
 exit 0;
